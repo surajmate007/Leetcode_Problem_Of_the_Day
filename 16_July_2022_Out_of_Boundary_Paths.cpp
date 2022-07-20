36f35aaaab46bf89851006360ff1ce8f4a27aaaf
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-07-20 15:48:21 +0530

    revise

--- 16_July_2022_Out_of_Boundary_Paths.cpp
+++ 16_July_2022_Out_of_Boundary_Paths.cpp
@@ -70,3 +70,72 @@ public:
         return getAns(m, n, startRow, startColumn, maxMove, dp);
     }
 };
+
+
+
+// GFG potd:
+// find smaller elemnets on the right of each element in the array.
+
+
+class Solution{
+public:
+    
+    void merge(vector<int> &count,vector<pair<int,int>> &v,int left ,int mid,int right){
+        
+        vector<pair<int,int>> temp(right-left+1);
+        int i=left;
+        int j=mid+1;
+        int k=0;
+        
+        while(i<=mid && j<=right){
+            
+            if(v[i].first <= v[j].first){
+                temp[k++] = v[j++];
+            }
+            else{
+                count[v[i].second] += right-j+1;
+                temp[k++]=v[i++];
+            }
+        }
+        
+        while(i<=mid){
+            temp[k++]=v[i++]; 
+        }
+        while(j<=right){
+            temp[k++]=v[j++];
+        }
+        for(int i=left;i<=right;i++){
+            v[i]=temp[i-left];
+        }
+    }
+    
+    void mergeSort(vector<int> &count,vector<pair<int,int>> &v,int left,int right){
+        
+        if(left<right){
+            int mid=left + (right-left)/2;
+            mergeSort(count,v,left,mid);
+            mergeSort(count,v,mid+1,right);
+            merge(count,v,left,mid,right);
+        }
+    }
+
+
+	vector<int> constructLowerArray(int *arr, int n) {
+	    
+	    vector<pair<int,int>>v(n);
+	    
+	    for(int i=0;i<n;i++){
+	        pair<int,int>p;
+	        p.first=arr[i];
+	        p.second=i;
+	        v[i]=p;
+	    }
+	    
+	    vector<int>count(n,0);
+	    
+	    mergeSort(count,v,0,n-1);
+	    
+	    return count;
+	    
+	}
+};
