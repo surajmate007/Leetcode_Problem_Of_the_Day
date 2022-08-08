c28a95b71fc50ad0fc77c402663163f5539afe4c
Suraj Kiran Mate <80340766+surajmate007@users.noreply.github.com>
2022-08-08 10:41:09 +0530

    Update 38_08_Aug_Count_Vowels_Permutations.cpp

--- 38_08_Aug_Count_Vowels_Permutations.cpp
+++ 38_08_Aug_Count_Vowels_Permutations.cpp
@@ -17,3 +17,16 @@ public:
     }
 };
 
+
+
+class Solution{   
+public:
+    int getOddOccurrence(int arr[], int n) {
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            ans = ans ^ arr[i];
+        }
+        
+        return ans;
+    }
+};
